aa626e8969424282faf176b1b6f23210cd1b2700
adriano-r <adrianorosa9974@gmail.com>
2022-01-20 17:47:22 -0300

    node API Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import "./app.scss";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
 
 const App = () => {
 	return (
 		<div>
-			<Login />
+			<Home />
 		</div>
 	);
 };
