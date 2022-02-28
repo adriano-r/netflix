55679bfb286faeffbbe65fca3663d3bb04fa083c
adriano-r <adrianorosa9974@gmail.com>
2022-02-28 00:29:28 -0300

    fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,45 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
+	const user = true;
 	return (
-		<div>
-			<Home />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={user ? <Home /> : <Navigate to="/register" />}
+				/>
+				<Route
+					exact
+					path="/register"
+					element={!user ? <Register /> : <Navigate to="/" />}
+				/>
+				<Route
+					exact
+					path="/login"
+					element={!user ? <Login /> : <Navigate to="/" />}
+				/>
+				{user && (
+					<>
+						<Route
+							path="/movies"
+							element={<Home type="movies" />}
+						/>
+						<Route
+							path="/series"
+							element={<Home type="series" />}
+						/>
+						<Route path="/watch" element={<Watch />} />
+					</>
+				)}
+			</Routes>
+		</BrowserRouter>
 	);
 };
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,12 +2,37 @@ import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import List from "../../components/list/List";
 import "./home.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({ type }) => {
+	const [lists, setLists] = useState([]);
+	const [genre, setGenre] = useState(null);
+
+	useEffect(() => {
+		const getRandomList = async () => {
+			try {
+				const res = await axios.get(
+					`lists ${type ? "?type=" + type : ""}${
+						genre ? "&genre=" + genre : ""
+					} `,
+					{
+						headers: {
+							token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWVlYzY0YzMzYmRkMzU3NGJlOWVjNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDM1MDIwNywiZXhwIjoxNjQ0NzgyMjA3fQ.7W1pdLhzQeWBPF-ajJsp3RNt5qc939z62ZP0o_L8CGE",
+						},
+					}
+				);
+				console.log(res);
+				// setLists(res.data);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+	}, [type, genre]);
 	return (
 		<div className="home">
 			<Navbar />
-			<Featured />
+			<Featured type={type} />
 			<List />
 			<List />
 			<List />
